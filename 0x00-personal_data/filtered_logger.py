7b9e8557905d75a239108a8c1c1311e389fa72dd
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2023-05-25 22:34:10 +0000

    tasks 1 - 4

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,34 +1,72 @@
+#!/usr/bin/env python3
+"""A module for filtering logs.
+"""
 import logging
+import csv
+import os
+import mysql.connector
 import re
+
 
 class RedactingFormatter(logging.Formatter):
     """Redacting Formatter class"""
 
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
+    def __init__(self, fields):
+        self.fields = fields
+        super(RedactingFormatter, self).__init__("[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s")
 
-    def __init__(self):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-
-    def format(self, record: logging.LogRecord) -> str:
-        fields = record.getMessage().split(self.SEPARATOR)
-        redacted_fields = filter_datum(fields, self.REDACTION, self.SEPARATOR)
-        record.msg = self.SEPARATOR.join(redacted_fields)
+    def format(self, record):
+        record.message = filter_datum(record.message, self.fields)
         return super().format(record)
 
+def filter_datum(message, fields):
+    for field in fields:
+        pattern = r"(?<!\w){}(?!\w)".format(re.escape(field))
+        message = re.sub(pattern, "***", message)
+    return message
 
-def filter_datum(fields, redaction, separator):
-    return re.sub(r'({0})(?={1})'.format('|'.join(fields), re.escape(separator)), redaction, separator.join(fields))
+def get_logger():
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
 
+    formatter = RedactingFormatter(PII_FIELDS)
 
-# Usage example
-logger = logging.getLogger("example_logger")
-formatter = RedactingFormatter()
-handler = logging.StreamHandler()
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
 
-# Log message example
-message = "Sensitive data: 123456789; Password: mypassword; API Key: abcdefg"
-logger.warning(message)
+    logger.addHandler(handler)
+
+    return logger
+
+def get_db():
+    credentials = {
+        "user": os.getenv("PERSONAL_DATA_DB_USERNAME"),
+        "password": os.getenv("PERSONAL_DATA_DB_PASSWORD"),
+        "host": os.getenv("PERSONAL_DATA_DB_HOST"),
+        "database": os.getenv("PERSONAL_DATA_DB_NAME"),
+    }
+    return mysql.connector.connect(**credentials)
+
+PII_FIELDS = ("email", "phone_number", "address", "credit_card", "social_security")
+
+def main():
+    db = get_db()
+    cursor = db.cursor()
+
+    query = "SELECT * FROM users"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    logger = get_logger()
+    logger.info("Filtered fields:\n")
+    for row in rows:
+        filtered_row = [row[field] for field in PII_FIELDS]
+        logger.info(filtered_row)
+
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    main()
+
