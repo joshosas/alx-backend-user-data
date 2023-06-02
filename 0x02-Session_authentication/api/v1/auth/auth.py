410f3ea2e0845b2a91b6ce25417267e748dac753
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2023-06-02 04:47:35 +0000

    complete mandatory tasks commit

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -37,3 +37,10 @@ class Auth:
         """Retrieves the current user from the request.
         """
         return None
+
+    def session_cookie(self, request=None) -> str:
+        """Gets the value of the cookie named 'SESSION_NAME'.
+        """
+        if request is not None:
+            cookie_name = os.getenv('SESSION_NAME')
+            return request.cookies.get(cookie_name)
